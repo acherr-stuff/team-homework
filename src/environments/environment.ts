c3e082eae9ec5b91e1564790275b6c774c38d80e
Anastasia <andreeva_98@inbox.ru>
2022-12-06 10:48:51 +0300

    added config for connection with server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: '127.0.0.1',
+  port: '8080'
 };
 
 /*
