24295674726e6937cda13b227b51f502c2fd0f4a
Anastasia <andreeva_98@inbox.ru>
2022-12-06 12:57:56 +0300

    edited service for data

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -9,50 +9,23 @@ import {BehaviorSubject, map, Observable, Subject} from "rxjs";
 })
 export class HttpService {
 
-   // generalData$: Subject<DataItem[]> = new Subject<DataItem[]>();
 
-    dataSubject = new BehaviorSubject([]);
-   // dataSubject$ = this.dataSubject.asObservable();
+    dataSubject$ = new BehaviorSubject([]);
     constructor(
         private http: HttpClient
     ) {
     }
 
-    // getGeneralData(): Observable<any> {
-    //     return this.http.get(`http://${environment.url}:${environment.port}/test_data_layer1.json`)
-    // };
-
-    // getGeneralData() {
-    //     this.http.get(`http://${environment.url}:${environment.port}/test_data_layer1.json`).subscribe((res) => {
-    //         let resSTR = JSON.stringify(res);
-    //         let resJSON = JSON.parse(resSTR);
-    //         this.generalData$.next(resJSON);
-    //         console.log(resJSON)
-    //         // this.generalData$.subscribe(value => {
-    //         //     console.log("subj: ", value)
-    //         // })
-    //         //console.log("subj: ", this.generalData$)
-    //         //console.log(resJSON);
-    //         //this.generalData(nex)
-    //         //return resJSON;
-    //     })
-    //
-    // }
-
-
-    //constructor(private apiService: ApiService) {}
 
     getGeneralData() {
-        this.http.get(`http://${environment.url}:${environment.port}/test_data_layer1.json`)
+        this.http.get(`http://${environment.url}:${environment.port}/test_data_layer0.json`)
             .pipe(
                 map(x => JSON.stringify(x)),
                 map(x => JSON.parse(x)),
-               // pluck("hydra:member")
             )
             .subscribe((data) => {
                 console.log(data);
-                this.dataSubject.next(data);
-               // console.log(this.dataSubject);
+                this.dataSubject$.next(data);
             });
     }
 }
