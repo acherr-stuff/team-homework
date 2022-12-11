bf408a4a2102b6c7e677fa612bbde07c222aa2b7
Anastasia <andreeva_98@inbox.ru>
2022-12-11 14:39:31 +0300

    edited methods in service and styles for table

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,34 +1,35 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {ChartDataInterface, ChartDatasetsInterface, DataItem, DataItemDetailed} from "../model/data-types";
 import {BehaviorSubject, map, Observable, Subject, Subscription} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class HttpService {
 
 
     dataSubject$ = new BehaviorSubject([]);
     sub?: Subscription;
+
     constructor(
         private http: HttpClient
     ) {
     }
 
-    getGeneralData():void {
+    getGeneralData(): void {
         this.getData('data')
     }
 
-    getDetailedData():void {
+    getDetailedData(): void {
         this.getData('data_detailed')
     }
 
 
-    getData(fileName: string):void {
+    getData(fileName: string): void {
         this.sub = this.http.get(`http://${environment.url}:${environment.port}/${fileName}`,
-            )
+        )
             .pipe(
                 map(x => JSON.stringify(x)),
                 map(x => JSON.parse(x)),
@@ -39,20 +40,15 @@ export class HttpService {
     }
 
 
-
     getDetailedDataByWHId(param: string, id: number, startDate?: string, endDate?: string): Observable<DataItemDetailed[]> {
+
+        let params = new HttpParams().set('wh_id', id)
         if (startDate && endDate) {
-            return this.http.get(`http://${environment.url}:${environment.port}/data_detailed`, {
-                params: new HttpParams().set('wh_id', id).set('dt_date_gte', startDate).set('dt_date_lte', endDate)
-            })
-                .pipe(
-                    map(x => JSON.stringify(x)),
-                    map(x => JSON.parse(x)),
-                )
-        } else
+            params.set('dt_date_gte', startDate).set('dt_date_lte', endDate);
+        }
 
         return this.http.get(`http://${environment.url}:${environment.port}/data_detailed`, {
-            params: new HttpParams().set('wh_id', id)
+            params: params
         })
             .pipe(
                 map(x => JSON.stringify(x)),
